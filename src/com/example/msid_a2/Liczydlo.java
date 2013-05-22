4bbc628e97e9261413da5fc70471172eea30a26c
Adam Borek <adambor3k@gmail.com>
2013-05-22 12:47:44 +0200

    Poprawki dla czytelnosci kodu

--- src/com/example/msid_a2/Liczydlo.java
+++ src/com/example/msid_a2/Liczydlo.java
@@ -37,9 +37,9 @@ public class Liczydlo {
 		 return result;
 	 }
 	
-	  public float getiMin(List<Point> pt) {
+	  public float getiMin(List<Point> points) {
          float imin = Integer.MAX_VALUE;
-          for(Point p : pt) {
+          for(Point p : points) {
               if(imin>p.x)
                   imin = p.x;
           }
@@ -47,11 +47,11 @@ public class Liczydlo {
           return imin;            
       }
       
-      public float getiMax(List<Point> pt) {
+      public float getiMax(List<Point> points) {
           
          float imax = Integer.MIN_VALUE;
           
-          for(Point p : pt) {
+          for(Point p : points) {
               if(imax<p.x)
                   imax = p.x;
           }
@@ -60,10 +60,10 @@ public class Liczydlo {
           
       }
       
-      public float getjMax(List<Point> pt) {
+      public float getjMax(List<Point> points) {
           
          float jmax = Integer.MIN_VALUE;
-          for(Point p : pt) {
+          for(Point p : points) {
               if(jmax<p.y)
                   jmax = p.y;
           }
@@ -71,11 +71,11 @@ public class Liczydlo {
           return jmax;   
       }
       
-      public float getjMin(List<Point> pt) {
+      public float getjMin(List<Point> points) {
           
          float jmin = Integer.MAX_VALUE;
           
-          for(Point p : pt) {
+          for(Point p : points) {
               if(jmin>p.y)
                   jmin = p.y;
           }
@@ -104,7 +104,7 @@ public class Liczydlo {
     	  return result;
       }
       
-      public double[] modelStat(Litera litera,int D,Matrix cechy) throws IllegalDimensionException, NoSquareException{
+      public double modelStat(Litera litera,int D,Matrix cechy) throws IllegalDimensionException, NoSquareException{
     	 if(cechy.getNrows() != D) throw new ArrayIndexOutOfBoundsException(cechy.getNrows());
     	 double[] wynik = new double[D];
     	 //Liczenie wykladnika; m3=(phi-mi)  m2=Ek^-1 m1=m3^T ; T-transponowane
@@ -112,8 +112,9 @@ public class Liczydlo {
     	 Matrix m2 = MatrixMathematics.inverse(litera.liczMacierzKowariancji(litera.srednia));
     	 Matrix m1 = MatrixMathematics.transpose(m3);
     	 double wyznacznik = MatrixMathematics.multiply(MatrixMathematics.multiply(m1, m2),m3).multiplyByConstant(-0.5).getValueAt(0, 0); // {-1/2 *m1*m2*m3}
+    	//Policzenie modelu
     	 double p = 1/(Math.pow(2*Math.PI,(double)D/2.0)*Math.sqrt(MatrixMathematics.determinant(litera.macierzKowariancji))) * Math.exp(wyznacznik);
-    	 return wynik;
+    	 return p;
       }
       
 }
